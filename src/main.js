a6811006be8462bcd8e85f14cd593a42d464916a
cedraz <67244541+cedraz@users.noreply.github.com>
2022-12-13 23:46:52 -0300

    teste do frontend

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+const input = document.querySelector('.pedro');
+
+const button = document.querySelector('.button');
+
+const resposta = document.querySelector('.resposta');
+
+button.addEventListener('click', () => {
+    const div = document.createElement('div')
+    div.innerHTML = Number(input.value) * 2
+    resposta.appendChild(div)
+});
+
+multiplica(Number(input.value));
