f97a0b44a4c5b628afce1a1013f514e183f47f00
KenzoVictor <satteliw@gmail.com>
2022-12-15 00:20:32 -0300

    bubblesort

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const input = document.querySelector('.pedro');
 const button = document.querySelector('.button');
 
 const resposta = document.querySelector('.resposta');
-
+ 
 button.addEventListener('click', () => {
     const div = document.createElement('div');
     div.innerHTML = Number(input.value) * 2;
