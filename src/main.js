f576036b0403aa33e2b23770255c3f0e068d5969
KenzoVictor <satteliw@gmail.com>
2022-12-15 00:51:05 -0300

    bubble sort complete

--- src/main.js
+++ src/main.js
@@ -1,13 +1,4 @@
-const input = document.querySelector('.pedro');
+import { bubbleSort } from "./modules/bubbleSort.js";
+let array = [3,1,4,2,7,6,9,8]
 
-const button = document.querySelector('.button');
-
-const resposta = document.querySelector('.resposta');
- 
-button.addEventListener('click', () => {
-    const div = document.createElement('div');
-    div.innerHTML = Number(input.value) * 2;
-    resposta.appendChild(div);
-});
-
-multiplica(Number(input.value));
+bubbleSort(array);
