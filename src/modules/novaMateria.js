6d8d9caac2057cdb151b5a4b10cdd3ff10699087
cedraz <67244541+cedraz@users.noreply.github.com>
2022-12-16 15:11:31 -0300

    incorporando a função media

--- src/modules/novaMateria.js
+++ src/modules/novaMateria.js
@@ -1,5 +1,6 @@
 export { novaMateria }
 import { media } from "./media";
+
 function novaMateria(nome, divPai) {
     const table = document.createElement('table');
     const tr = document.createElement('tr');
@@ -12,7 +13,7 @@ function novaMateria(nome, divPai) {
     const pe3  =document.createElement('input');
     const ch  =document.createElement('input');
 
-    const mediaDaMateria = media([p1.value, p2.value, p3.value], [pe1.value, pe2.value, pe3.value])
+    const mediaDaMateria = media([p1.value, p2.value, p3.value], [pe1.value, pe2.value, pe3.value]);
 
     nomeMateria.innerHTML = nome
     const array = [nomeMateria, p1, p2, p3, pe1, pe2, pe3, ch];
