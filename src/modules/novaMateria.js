420fb293cc9d7ee2b19166ac1199490007c04823
cedraz <67244541+cedraz@users.noreply.github.com>
2022-12-17 19:16:00 -0300

    type number do input

--- src/modules/novaMateria.js
+++ src/modules/novaMateria.js
@@ -12,19 +12,20 @@ function novaMateria(nome, divPai) {
     const pe3 = document.createElement('input');
     const ch = document.createElement('input');
 
-    const mediaDaMateria = media([p1.value, p2.value, p3.value], [pe1.value, pe2.value, pe3.value])
+    const mediaDaMateria = media([p1.value, p2.value, p3.value], [pe1.value, pe2.value, pe3.value]);
 
-    nomeMateria.innerHTML = nome
+    nomeMateria.innerHTML = nome;
     const array = [p1, p2, p3, pe1, pe2, pe3, ch];
     
     tr.appendChild(nomeMateria)
     for (let tag of array) {
-        tr.appendChild(tag)
+        tag.type = "number"
+        tr.appendChild(tag);
     }
     
     table.appendChild(tr);
     divPai.appendChild(table);
-    return array
+    return array;
 }
 
 export { novaMateria }
